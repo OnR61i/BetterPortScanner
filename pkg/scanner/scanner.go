0f5b7004bbf2dad72aee0c08c631b42fa4fc71b5
Cursage <random.spam.germany@gmail.com>
2026-04-20 12:39:00 +0200

    Feature implemented: Resize Workerpool

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -12,17 +12,26 @@ type Scanner struct{
  	strategy Strategy 
 	chan jobs Job
 	chan results Job
-	chan stopChan struct{}
+	chan stopChan bool
 	wg *sync.WaitGroup
 	status string
 }
 
 
 func NewScanner(activeWorkers int) Scanner{
-
+	return Scanner{
+			jobs: 		make(chan, Job),
+			results:	make(chan, Job),
+			stopChan:	make(chan, bool),
+			wg: 		*sync.WaitGroup{},
+			status:		"waiting",
+	}
 }
 
-func initWorker(){
+func (scanner *Scanner) initWorker(workerId int){
+	for(workerId >= scanner.activeWorkers){
+		// Executing request...
+	}
 }
 
 func (scanner *Scanner) startScan(target []net.IP, portRng []int, strategy Strategy) error{
@@ -40,11 +49,19 @@ func (scanner *Scanner) startScan(target []net.IP, portRng []int, strategy Strat
 func (scanner *Scanner) stopScan(){
 }
 
-func (scanner *Scanner) resizeWorkerPool(newCount int){
+func (scanner *Scanner) resizeWorkerPool(newAmount int){
+
+	formerAmount := scanner.activeWorkers
+
+	scanner.activeWorkers = newAmount	
+	
+	// Creation count of workers are their id...	
+	id := formerAmount	
+	for(formerAmount < newAmount){
+		id++
+		go initWorker(id)
+	}
 }
 
-// Required setter-methods...
 
-func (scanner *Scanner) setStrategy(strategy Strategy){
-}
 

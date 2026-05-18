e83d870c580bcbc0c9d549524a59306e15f90c55
Cursage <random.spam.germany@gmail.com>
2026-05-18 14:50:07 +0200

    Declared func 'ExecScan' -not_tested

--- pkg/ui/bridge.go
+++ pkg/ui/bridge.go
@@ -1,6 +1,10 @@
-package pkg/ui
+package ui
 
 import(
+	"pkg/scanner"
+	"net"
+	"errors"
+	"sync"
 )
 
 type Bridge struct{
@@ -10,15 +14,46 @@ type Bridge struct{
 	configFile ConfigFile
 	totalJobs int
 	currentResults []Job
+	wg *sync.WaitGroup
 }
 
-func NewBridge() Bridge{
+func NewBridge(scanner *scanner.Scanner, window *window, config *Config, configFile ConfigFile) Bridge{
+	wg := sync.WaitGroup{}	
+	return Bridge{
+		scanner: 	scanner,
+		window:	 	window,
+		config:	 	config,
+		configFile:	configFile,
+		wg:		wg,
+	}
 }
 
 func ExecuteScan(targetRange string, PortRange string, strategy int) error{
+	// Checking if target range is a CIDR...
+	ip, ipnet, err := net.parseCIDR(targetRange)
+	if err != nil {
+		// Checking if target is a single IP...
+		ip := net.parseIP(targetRange) 
+		if ip = nil{
+			return errors.New("Given target range invalid") 
+		}
+
+		target := []net.IP{ip}
+		scanner.Scan(target, []int, scanner.Strategy, &wg)
+		return nil	
+	}
+	// Iterating over valid target range...	
+	var targets []net.IP	
+	for ip := ipnet.IP; ipnet.Contains(ip); inc(ip) {
+		ipRange := append(ipRange, ip)
+	}
+	
+	scanner.Scan(targets, []int, scanner.Strategy, &wg)
+	return nil	
 }
 
 func StopScan(){
+	scanner.Interrupt()	
 }
 
 func listenForResults(){
@@ -26,3 +61,13 @@ func listenForResults(){
 
 func calculateProgress() float{
 }
+
+// Function for iterating over IP range...
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j--{
+		ip[j]++
+		if ip[j] != 0 {
+			break
+		}
+	}
+} 
